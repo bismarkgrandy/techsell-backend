ea74fedd65ed925543c0381cb2f8111e63551681
bismark <bismarkakwah86@gmail.com>
2025-03-24 10:29:23 +0000

    cookies issue fixed

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -8,8 +8,8 @@ const generateToken = async(userId, res)=>{
  res.cookie("jwt",token, {
     maxAge:7*24*60*60*1000,
     httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !== "development"
+    sameSite:"none",
+    secure:process.env.NODE_ENV !== "production"
  });
 
  return token;
