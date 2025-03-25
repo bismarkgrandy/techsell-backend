9c039ea46f975f050add959844bdddd0bbea6e2d
bismark <bismarkakwah86@gmail.com>
2025-03-25 10:06:13 +0000

    cookies fixed in the backend

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -9,7 +9,7 @@ const generateToken = async(userId, res)=>{
     maxAge:7*24*60*60*1000,
     httpOnly:true,
     sameSite:"none",
-    secure:process.env.NODE_ENV !== "production"
+    secure:process.env.NODE_ENV === "production"
  });
 
  return token;
